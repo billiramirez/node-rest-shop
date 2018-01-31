ea3e58eff436fc6887b69a59829a916bd3f87689
Billi Ramirez <ramirezbilly42@outlook.com>
2018-01-31 15:07:23 -0600

    submiting the project

---
+++ api/models/products.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    price: { type: Number, required: true }
+});
+
+//Exportamos el modelo, se recomienda utilizar Capitalizacion y le pasamos el esquema creado.
+module.exports = mongoose.model('Product', productSchema);
