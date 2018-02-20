2d7f30cfa2723275b1cd6bdd607a428209059b8a
Billi Ramirez <ramirezbilly42@outlook.com>
2018-02-20 14:03:03 -0600

    The jwt, adding the token, besides there is a issue with the process.env.JWT_KEY is not correctly readble , We had to hardcoded the secretOrPrivate key

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 
 const User = require('../models/user');
+
+
 
 
 router.post('/signup', (req, res, next) => {
@@ -49,6 +53,7 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', (req, res, next) => {
+    /**Lets find if a user is into the database */
     User.find({email: req.body.email})
     .exec()
     .then(user => {
@@ -58,14 +63,30 @@ router.post('/login', (req, res, next) => {
             });
         }
         bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            /**If the password failed */
             if(err){
                 return res.status(401).json({
                     message: 'Auth Failed'
                 });
             }
+            /***IF the password is correct */
+            /**Lets create the token   */
             if(result) {
+                const token = jwt.sign(
+                {
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, 
+                
+                "ans5sfa4sfgassdk",
+                {
+                    expiresIn: "1h"
+                }
+                );
+                //console.log(process.env);
                 return res.status(200).json({
-                    message: 'Auth successful'
+                    message: 'Auth successful',
+                    token: token
                 });
             }
             return res.status(401).json({
