fbbe15dd562082dbb912fc042f0454a6167a9e4c
Billi Ramirez <ramirezbilly42@outlook.com>
2018-02-19 19:29:16 -0600

    adding user signup with the delete route and regex for the email on the user schema definition

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,19 +1,68 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
 
 router.post('/signup', (req, res, next) => {
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: req.body.password
-    });
+    /**Let's find if a user has been created with the same email address */
+    User.find({email: req.body.email})
+    .exec()
+    .then(user => {
+        if (user.length >= 1){
+            res.status(409).json({
+                messsage: 'Email exists'
+            })
+        } else {
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                    return res.status(500).json({
+                        error: err
+                    });
+                } else {
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password: hash
+                    });
+                    user.save()
+                        .then(result => {
+                            console.log(result);
+                            res.status(201).json({
+                                messsage: 'User Created'
+                            })
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+                            })
+                        });
+                }
+            });
+        }
+    })
+    
+    
 });
 
-
+router.delete('/:userId', (req, res, next) => {
+    User.remove({ _id: req.params.userId})
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message: 'USer deleted'
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
 
 
 
--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-
+/*Importing the routes from the route folder */
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
+/******************************************************* */
 
 mongoose.connect('mongodb://node-shop:'+ process.env.MONGO_ATLAS_PW +'@node-rest-shop-shard-00-00-p59pl.mongodb.net:27017,node-rest-shop-shard-00-01-p59pl.mongodb.net:27017,node-rest-shop-shard-00-02-p59pl.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin');
 mongoose.Promise = global.Promise;
@@ -15,7 +17,7 @@ app.use(morgan('dev'));
 app.use('/uploads',express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
+/***CORS Error preventions */
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
@@ -28,11 +30,12 @@ app.use((req, res, next) => {
     }
     next();
 });
-
+/******************************************************************************************* */
 
 // Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders',orderRoutes);
+app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
    const error = new Error('Not found');
